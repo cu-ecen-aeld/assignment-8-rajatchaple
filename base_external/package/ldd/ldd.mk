e8b58c2d6496c1514e47c41f44941345a5b8a3c6
rajat <rajat.chaple@yahoo.com>
2021-10-16 23:02:25 -0600

    changes for assignment 7 part 2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = '29ffbd2a359abb30da1dfb887162e178d5d05e4b'
+LDD_VERSION = 'e58a3c2c05e78abb304633c6e35815637e111334'
 # Note:: Be sure to reference the *ssh* repository URL here (not https) to work properly                                                                                                                    
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -13,6 +13,9 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-rajatchaple.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
+define AESD_ASSIGNMENTS_BUILD_CMDS
+  $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app clean
+endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define LDD__INSTALL_TARGET_CMDS
@@ -22,5 +25,8 @@ define LDD__INSTALL_TARGET_CMDS
   $(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
   
 endef
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
