33bfeff4eeb39122a6e599630deacc287becadfa
rajat <rajat.chaple@yahoo.com>
2021-10-17 12:22:30 -0600

    Assign 7 part 2 buildroot

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,16 +5,16 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = 'e58a3c2c05e78abb304633c6e35815637e111334'
+LDD_VERSION = 'e42ad03c70710e99b113157f1e5a277f96398f15'
 # Note:: Be sure to reference the *ssh* repository URL here (not https) to work properly                                                                                                                    
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-rajatchaple.git'
 LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
+#LDD_GIT_SUBMODULES = YES
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define LDD__INSTALL_TARGET_CMDS
+define LDD_INSTALL_TARGET_CMDS
   $(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
   $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
   $(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
@@ -23,6 +23,7 @@ define LDD__INSTALL_TARGET_CMDS
 endef
 LDD_MODULE_SUBDIRS = misc-modules
 LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
